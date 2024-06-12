26d3845b4b9d74d5b1ce4a2d94ad489047870aac
Dita <ditatianto@gmail.com>
2024-06-12 12:36:15 +0700

    membuat controller update_layanan

---
+++ src/Controller/Update_Layanan.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import Model.Layanan;
+
+public class Update_Layanan {
+    public Update_Layanan(int give_id_layanan, String give_layanan, String give_deskripsi, double give_harga, int give_id_kategori){
+        Layanan up_layanan = new Layanan();
+        up_layanan.set_data_layanan(give_layanan, give_deskripsi, give_harga, give_id_kategori);
+        up_layanan.update_layanan(give_id_layanan);
+    }
+    
+}
